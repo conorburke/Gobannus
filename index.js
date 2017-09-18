4f9481a7409e6ebc08e724e4c989eed47506e7a8
conorburke <cjburke89@gmail.com>
2017-09-17 19:57:45 -0700

    set up basic passport google strategy with callback

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-
+const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET;
 const PORT = process.env.PORT;
+
+passport.use(new GoogleStrategy({
+	clientID: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET,
+	callbackURL: '/auth/google/callback'
+	}, (accessToken, refreshToken, profile, done) => {
+		console.log('access token', accessToken);
+		console.log('refresh token', refreshToken);
+		console.log('profile', profile);
+	})
+);
+
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
 });
@@ -11,3 +26,12 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
 	res.send({new: 'Hello World'});
 });
+
+//send user to google page to ask for authentication
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}));
+
+//handle request after user authorizes Gobannus to use google account
+//passprt sees code inside url and can handle it
+app.get('/auth/google/callback', passport.authenticate('google'));
