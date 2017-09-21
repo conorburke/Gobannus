fc0854bf51813a165fc68211ea83275adedf20c3
conorburke <cjburke89@gmail.com>
2017-09-20 19:51:54 -0700

    make minor adjustments to reorder code

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ require('./services/passport');
 const app = express();
 const PORT = process.env.PORT;
 
+// mongoose.connect(`${process.env.MONGO_URI}`);
+let mongoPromise = mongoose.connect(`${process.env.MONGO_URI}`, {
+	useMongoClient: true
+});
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
+
 app.use(cookieSession({
 	//expires in 30 days
 	maxAge: 1000 * 60 * 60 * 24 * 30,
@@ -23,15 +32,6 @@ app.use(cookieSession({
 //get passport to use cookies
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.listen(PORT, () => {
-	console.log(`listening on port ${PORT}`);
-});
-
-// mongoose.connect(`${process.env.MONGO_URI}`);
-let mongoPromise = mongoose.connect(`${process.env.MONGO_URI}`, {
-	useMongoClient: true
-});
 
 //immediately invoke auth routes
 require('./routes/authRoutes')(app);
