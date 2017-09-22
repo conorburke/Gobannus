4378f9958c783e6c470de9c2f08f594d9ef5afb1
conorburke <cjburke89@gmail.com>
2017-09-21 21:48:14 -0700

    change index route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,6 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send({welcome: 'To Gobannus'});
-    });
 
     //send user to google page to ask for authentication
     app.get('/auth/google', passport.authenticate('google', {
